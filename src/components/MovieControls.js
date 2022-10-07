90c928926a18f5c8af8e9f82927e6ad941758d82
Ronaldkubasu <ronald.kubasu@student.moringaschool.com>
2022-10-07 13:32:43 +0300

    added a section

--- src/components/MovieControls.js
+++ src/components/MovieControls.js
@@ -10,7 +10,20 @@ export const MovieControls = ({ type, movie }) => {
   } = useContext(GlobalContext);
 
   return (
-    
+    <div className="inner-card-controls">
+      {type === "watchlist" && (
+        <>
+          <button className="ctrl-btn" onClick={() => addMovieToWatched(movie)}>
+            <i className="fa-fw far fa-eye"></i>
+          </button>
+
+          <button
+            className="ctrl-btn"
+            onClick={() => removeMovieFromWatchlist(movie.id)}
+          >
+            <i className="fa-fw fa fa-times"></i>
+          </button>
+        </>
       )}
 
       {type === "watched" && (
