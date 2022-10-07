1729dc80324daf06f475064208ee5081b2bee2d7
Ronaldkubasu <ronald.kubasu@student.moringaschool.com>
2022-10-07 13:48:36 +0300

    added a button

--- src/components/MovieControls.js
+++ src/components/MovieControls.js
@@ -27,18 +27,18 @@ export const MovieControls = ({ type, movie }) => {
       )}
 
       {type === "watched" && (
-        // <>
-        //   <button className="ctrl-btn" onClick={() => moveToWatchlist(movie)}>
-        //     <i className="fa-fw far fa-eye-slash"></i>
-        //   </button>
+        <>
+          <button className="ctrl-btn" onClick={() => moveToWatchlist(movie)}>
+            <i className="fa-fw far fa-eye-slash"></i>
+          </button>
 
-        //   <button
-        //     className="ctrl-btn"
-        //     onClick={() => removeFromWatched(movie.id)}
-        //   >
-        //     <i className="fa-fw fa fa-times"></i>
-        //   </button>
-        // </>
+          <button
+            className="ctrl-btn"
+            onClick={() => removeFromWatched(movie.id)}
+          >
+            <i className="fa-fw fa fa-times"></i>
+          </button>
+        </>
       )}
     </div>
   );
