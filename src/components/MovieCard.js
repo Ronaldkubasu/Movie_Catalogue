3d24d357c0a79f4d38313d77e8bcd51c3982cc42
Ronaldkubasu <ronald.kubasu@student.moringaschool.com>
2022-10-07 14:07:32 +0300

    added a section

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,16 +2,16 @@ import React from "react";
 import { MovieControls } from "./MovieControls";
 
 export const MovieCard = ({ movie, type }) => {
-  return (
-    <div className="movie-card">
-      <div className="overlay"></div>
+  // return (
+  //   <div className="movie-card">
+  //     <div className="overlay"></div>
 
-      <img
-        src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-        alt={`${movie.title} Poster`}
-      />
+  //     <img
+  //       src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+  //       alt={`${movie.title} Poster`}
+  //     />
 
-      <MovieControls type={type} movie={movie} />
-    </div>
-  );
+  //     <MovieControls type={type} movie={movie} />
+  //   </div>
+  // );
 };
